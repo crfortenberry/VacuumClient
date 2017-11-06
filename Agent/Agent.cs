a2382bccdaa7125b0f21ea77a41112593127137a
crfortenberry <33079100+crfortenberry@users.noreply.github.com>
2017-11-06 15:22:56 -0600

    Add files via upload

--- Agent/Agent.cs
+++ Agent/Agent.cs
@@ -6,13 +6,38 @@ namespace IntelligentVacuum.Agent
 
     public class Agent
     {
+        private Random rand = new Random();
+        
         public Agent()
         {
         }
 
         public AgentAction DecideAction(Room room)
         {
-            return AgentAction.NONE;
+            AgentAction action = new AgentAction();
+
+            if (room.IsDirty){
+                action = AgentAction.CLEAN;
+            } else {
+                int direction = rand.Next(0,4);
+                switch (direction)
+                {
+                    case 0:
+                        action = AgentAction.MOVE_UP;
+                        break;
+                    case 1:
+                        action = AgentAction.MOVE_LEFT;
+                        break;
+                    case 2:
+                        action = AgentAction.MOVE_DOWN;
+                        break;
+                    case 3:
+                    default:
+                        action = AgentAction.MOVE_RIGHT;
+                        break;
+                }
+            }
+            return action;
         }
     }
 }
